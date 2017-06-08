a6588bd64a25c7130b3158b8dff5908c31d47990
Luc Chmielowski <luc.chmielowski@gmail.com>
2017-06-08 11:31:27 +0200

    Implemented service methods bis

--- service.go
+++ service.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+
+	empty "github.com/golang/protobuf/ptypes/empty"
 	pb "github.com/iochti/thing-service/proto"
 	"golang.org/x/net/context"
 )
@@ -12,26 +15,60 @@ type ThingSvc struct {
 
 // GetThing gets a thing by its ID and returns it
 func (t *ThingSvc) GetThing(ctx context.Context, in *pb.ThingIDRequest) (*pb.Thing, error) {
-	return nil, nil
+	if in.GetID() == 0 {
+		return nil, fmt.Errorf("Error: missing ID in request")
+	}
+	thingID := in.GetID()
+	tng := new(pb.Thing)
+	if err := t.Db.GetThingByID(thingID, tng); err != nil {
+		return nil, err
+	}
+	return tng, nil
 }
 
 // CreateThing takes a thing as parameter, creates it and returns it with its ID
 func (t *ThingSvc) CreateThing(ctx context.Context, in *pb.Thing) (*pb.Thing, error) {
-	return nil, nil
+	if err := t.Db.CreateThing(in); err != nil {
+		return nil, err
+	}
+
+	if in.GetID() == 0 {
+		return nil, fmt.Errorf("Error: no userid after insertion")
+	}
+	return in, nil
 }
 
 // UpdateThing takes a thing as parameter, updates the corresponding data
 // and returns the updated element
 func (t *ThingSvc) UpdateThing(ctx context.Context, in *pb.Thing) (*pb.Thing, error) {
-	return nil, nil
+	if in.GetID() == 0 {
+		return nil, fmt.Errorf("Error: can't update user with no ID")
+	}
+	if err := t.Db.UpdateThing(in); err != nil {
+		return nil, err
+	}
+	return in, nil
 }
 
 // DeleteThing takes a Thing ID as parameter, deletes it and returns the id of the deleted value
-func (t *ThingSvc) DeleteThing(ctx context.Context, in *pb.ThingIDRequest) (*pb.ThingDeletion, error) {
-	return nil, nil
+func (t *ThingSvc) DeleteThing(ctx context.Context, in *pb.ThingIDRequest) (*empty.Empty, error) {
+	if in.GetID() == 0 {
+		return nil, fmt.Errorf("Error: invalid id as parameter")
+	}
+	if err := t.Db.DeleteThing(in.GetID()); err != nil {
+		return nil, err
+	}
+	return new(empty.Empty), nil
 }
 
 // BulkDeleteThing takes a Thing array as parametes
-func (t *ThingSvc) BulkDeleteThing(ctx context.Context, in *pb.ThingIDArray) (*pb.ThingDeletion, error) {
-	return nil, nil
+func (t *ThingSvc) BulkDeleteThing(ctx context.Context, in *pb.ThingIDArray) (*empty.Empty, error) {
+	if len(in.GetThings()) == 0 {
+		return nil, fmt.Errorf("Error: empty ids array")
+	}
+
+	if err := t.Db.DeleteThingArray(in.GetThings()); err != nil {
+		return nil, err
+	}
+	return new(empty.Empty), nil
 }
