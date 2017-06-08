e0ee41ac87c6ea7c7a4800c9c9058be0677fd565
Luc Chmielowski <luc.chmielowski@gmail.com>
2017-06-08 12:34:57 +0200

    Modified proto to return empty on delete

--- main.go
+++ main.go
@@ -1,3 +1,57 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+
+	pb "github.com/iochti/thing-service/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+// Db is a PostgresDL instance used by the service
+var Db *PostgresDL
+
+func init() {
+	Db = new(PostgresDL)
+	Db.Init()
+}
+
+func dieIf(err error) {
+	if err == nil {
+		return
+	}
+	fmt.Fprintf(os.Stderr, "Error: %s. Try --help for help.\n", err)
+	os.Exit(-1)
+}
+
+func main() {
+	addr := flag.String("srv", ":5002", "TCP address to listen on (in host:port form)")
+	certFile := flag.String("cert", "", "Path to PEM-encoded certificate")
+	keyFile := flag.String("key", "", "Path to PEM-encoded secret key")
+	flag.Parse()
+	svc := &ThingSvc{
+		Db: Db,
+	}
+	var server *grpc.Server
+
+	// Create server, with TLS if cert & key are specified
+	if *keyFile == "" && *certFile == "" {
+		server = grpc.NewServer()
+	} else if *keyFile == "" {
+		dieIf(fmt.Errorf("Cert specified with no key"))
+	} else if *certFile == "" {
+		dieIf(fmt.Errorf("Key specified with no cert"))
+	} else {
+		pair, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		dieIf(err)
+		creds := grpc.Creds(pair)
+		server = grpc.NewServer(creds)
+	}
+	lis, err := net.Listen("tcp", *addr)
+	dieIf(err)
+	pb.RegisterThingSvcServer(server, svc)
+	server.Serve(lis)
+}
