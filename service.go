8e2ff483f5c3f5027b0877e57a3fc1bebdbe6767
Luc Chmielowski <luc.chmielowski@gmail.com>
2017-06-08 17:22:12 +0200

    Modified error return in to grpc.Error

--- service.go
+++ service.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	empty "github.com/golang/protobuf/ptypes/empty"
 	pb "github.com/iochti/thing-service/proto"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
 // ThingSvc implements the RPCs from ThingSvc
@@ -16,12 +16,12 @@ type ThingSvc struct {
 // GetThing gets a thing by its ID and returns it
 func (t *ThingSvc) GetThing(ctx context.Context, in *pb.ThingIDRequest) (*pb.Thing, error) {
 	if in.GetID() == 0 {
-		return nil, fmt.Errorf("Error: missing ID in request")
+		return nil, grpc.Errorf(codes.Internal, "Error: missing ID in request")
 	}
 	thingID := in.GetID()
 	tng := new(pb.Thing)
 	if err := t.Db.GetThingByID(thingID, tng); err != nil {
-		return nil, err
+		return nil, grpc.Errorf(codes.NotFound, err.Error())
 	}
 	return tng, nil
 }
@@ -29,11 +29,11 @@ func (t *ThingSvc) GetThing(ctx context.Context, in *pb.ThingIDRequest) (*pb.Thi
 // CreateThing takes a thing as parameter, creates it and returns it with its ID
 func (t *ThingSvc) CreateThing(ctx context.Context, in *pb.Thing) (*pb.Thing, error) {
 	if err := t.Db.CreateThing(in); err != nil {
-		return nil, err
+		return nil, grpc.Errorf(codes.Internal, err.Error())
 	}
 
 	if in.GetID() == 0 {
-		return nil, fmt.Errorf("Error: no userid after insertion")
+		return nil, grpc.Errorf(codes.Internal, "Error: no userid after insertion")
 	}
 	return in, nil
 }
@@ -42,10 +42,10 @@ func (t *ThingSvc) CreateThing(ctx context.Context, in *pb.Thing) (*pb.Thing, er
 // and returns the updated element
 func (t *ThingSvc) UpdateThing(ctx context.Context, in *pb.Thing) (*pb.Thing, error) {
 	if in.GetID() == 0 {
-		return nil, fmt.Errorf("Error: can't update user with no ID")
+		return nil, grpc.Errorf(codes.InvalidArgument, "Error: can't update user with no ID")
 	}
 	if err := t.Db.UpdateThing(in); err != nil {
-		return nil, err
+		return nil, grpc.Errorf(codes.Internal, err.Error())
 	}
 	return in, nil
 }
@@ -53,10 +53,10 @@ func (t *ThingSvc) UpdateThing(ctx context.Context, in *pb.Thing) (*pb.Thing, er
 // DeleteThing takes a Thing ID as parameter, deletes it and returns the id of the deleted value
 func (t *ThingSvc) DeleteThing(ctx context.Context, in *pb.ThingIDRequest) (*empty.Empty, error) {
 	if in.GetID() == 0 {
-		return nil, fmt.Errorf("Error: invalid id as parameter")
+		return nil, grpc.Errorf(codes.InvalidArgument, "Error: invalid id as parameter")
 	}
 	if err := t.Db.DeleteThing(in.GetID()); err != nil {
-		return nil, err
+		return nil, grpc.Errorf(codes.Internal, err.Error())
 	}
 	return new(empty.Empty), nil
 }
@@ -64,11 +64,11 @@ func (t *ThingSvc) DeleteThing(ctx context.Context, in *pb.ThingIDRequest) (*emp
 // BulkDeleteThing takes a Thing array as parametes
 func (t *ThingSvc) BulkDeleteThing(ctx context.Context, in *pb.ThingIDArray) (*empty.Empty, error) {
 	if len(in.GetThings()) == 0 {
-		return nil, fmt.Errorf("Error: empty ids array")
+		return nil, grpc.Errorf(codes.Internal, "Error: empty ids array")
 	}
 
 	if err := t.Db.DeleteThingArray(in.GetThings()); err != nil {
-		return nil, err
+		return nil, grpc.Errorf(codes.Internal, err.Error())
 	}
 	return new(empty.Empty), nil
 }
