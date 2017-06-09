8c99c6292ec83664e39d82085050e88db7f5440b
Luc Chmielowski <luc.chmielowski@gmail.com>
2017-06-09 12:45:34 +0200

    Modified proto

--- proto/thing.pb.go
+++ proto/thing.pb.go
@@ -3,7 +3,7 @@
 // DO NOT EDIT!
 
 /*
-Package proto is a generated protocol buffer package.
+Package iochti_thing is a generated protocol buffer package.
 
 It is generated from these files:
 	thing.proto
@@ -11,11 +11,12 @@ It is generated from these files:
 It has these top-level messages:
 	ThingIDArray
 	Thing
+	GroupIDRequest
 	ThingIDRequest
 */
-package proto
+package iochti_thing
 
-import proto1 "github.com/golang/protobuf/proto"
+import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
 import google_protobuf "github.com/golang/protobuf/ptypes/timestamp"
@@ -27,7 +28,7 @@ import (
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
-var _ = proto1.Marshal
+var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
 
@@ -35,14 +36,14 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto1.ProtoPackageIsVersion2 // please upgrade the proto package
+const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
 type ThingIDArray struct {
 	Things []int32 `protobuf:"varint,1,rep,packed,name=things" json:"things,omitempty"`
 }
 
 func (m *ThingIDArray) Reset()                    { *m = ThingIDArray{} }
-func (m *ThingIDArray) String() string            { return proto1.CompactTextString(m) }
+func (m *ThingIDArray) String() string            { return proto.CompactTextString(m) }
 func (*ThingIDArray) ProtoMessage()               {}
 func (*ThingIDArray) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
 
@@ -54,21 +55,29 @@ func (m *ThingIDArray) GetThings() []int32 {
 }
 
 type Thing struct {
-	ID          int32                      `protobuf:"varint,1,opt,name=ID" json:"ID,omitempty"`
-	Name        string                     `protobuf:"bytes,2,opt,name=name" json:"name,omitempty"`
-	Description string                     `protobuf:"bytes,3,opt,name=description" json:"description,omitempty"`
-	CreatedAt   *google_protobuf.Timestamp `protobuf:"bytes,4,opt,name=created_at,json=createdAt" json:"created_at,omitempty"`
-	UpdatedAt   *google_protobuf.Timestamp `protobuf:"bytes,5,opt,name=updated_at,json=updatedAt" json:"updated_at,omitempty"`
+	ID        int32                      `protobuf:"varint,1,opt,name=ID" json:"ID,omitempty"`
+	GroupID   int32                      `protobuf:"varint,2,opt,name=GroupID" json:"GroupID,omitempty"`
+	Name      string                     `protobuf:"bytes,3,opt,name=name" json:"name,omitempty"`
+	MAC       string                     `protobuf:"bytes,4,opt,name=MAC" json:"MAC,omitempty"`
+	CreatedAt *google_protobuf.Timestamp `protobuf:"bytes,5,opt,name=created_at,json=createdAt" json:"created_at,omitempty"`
+	UpdatedAt *google_protobuf.Timestamp `protobuf:"bytes,6,opt,name=updated_at,json=updatedAt" json:"updated_at,omitempty"`
 }
 
 func (m *Thing) Reset()                    { *m = Thing{} }
-func (m *Thing) String() string            { return proto1.CompactTextString(m) }
+func (m *Thing) String() string            { return proto.CompactTextString(m) }
 func (*Thing) ProtoMessage()               {}
 func (*Thing) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
 
 func (m *Thing) GetID() int32 {
 	if m != nil {
 		return m.ID
+	}
+	return 0
+}
+
+func (m *Thing) GetGroupID() int32 {
+	if m != nil {
+		return m.GroupID
 	}
 	return 0
 }
@@ -80,9 +89,9 @@ func (m *Thing) GetName() string {
 	return ""
 }
 
-func (m *Thing) GetDescription() string {
+func (m *Thing) GetMAC() string {
 	if m != nil {
-		return m.Description
+		return m.MAC
 	}
 	return ""
 }
@@ -101,14 +110,30 @@ func (m *Thing) GetUpdatedAt() *google_protobuf.Timestamp {
 	return nil
 }
 
+type GroupIDRequest struct {
+	ID int32 `protobuf:"varint,1,opt,name=ID" json:"ID,omitempty"`
+}
+
+func (m *GroupIDRequest) Reset()                    { *m = GroupIDRequest{} }
+func (m *GroupIDRequest) String() string            { return proto.CompactTextString(m) }
+func (*GroupIDRequest) ProtoMessage()               {}
+func (*GroupIDRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
+
+func (m *GroupIDRequest) GetID() int32 {
+	if m != nil {
+		return m.ID
+	}
+	return 0
+}
+
 type ThingIDRequest struct {
 	ID int32 `protobuf:"varint,1,opt,name=ID" json:"ID,omitempty"`
 }
 
 func (m *ThingIDRequest) Reset()                    { *m = ThingIDRequest{} }
-func (m *ThingIDRequest) String() string            { return proto1.CompactTextString(m) }
+func (m *ThingIDRequest) String() string            { return proto.CompactTextString(m) }
 func (*ThingIDRequest) ProtoMessage()               {}
-func (*ThingIDRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
+func (*ThingIDRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{3} }
 
 func (m *ThingIDRequest) GetID() int32 {
 	if m != nil {
@@ -118,9 +143,10 @@ func (m *ThingIDRequest) GetID() int32 {
 }
 
 func init() {
-	proto1.RegisterType((*ThingIDArray)(nil), "proto.ThingIDArray")
-	proto1.RegisterType((*Thing)(nil), "proto.Thing")
-	proto1.RegisterType((*ThingIDRequest)(nil), "proto.ThingIDRequest")
+	proto.RegisterType((*ThingIDArray)(nil), "iochti.thing.ThingIDArray")
+	proto.RegisterType((*Thing)(nil), "iochti.thing.Thing")
+	proto.RegisterType((*GroupIDRequest)(nil), "iochti.thing.GroupIDRequest")
+	proto.RegisterType((*ThingIDRequest)(nil), "iochti.thing.ThingIDRequest")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -135,6 +161,7 @@ const _ = grpc.SupportPackageIsVersion4
 
 type ThingSvcClient interface {
 	GetThing(ctx context.Context, in *ThingIDRequest, opts ...grpc.CallOption) (*Thing, error)
+	ListGroupThings(ctx context.Context, in *GroupIDRequest, opts ...grpc.CallOption) (ThingSvc_ListGroupThingsClient, error)
 	CreateThing(ctx context.Context, in *Thing, opts ...grpc.CallOption) (*Thing, error)
 	UpdateThing(ctx context.Context, in *Thing, opts ...grpc.CallOption) (*Thing, error)
 	DeleteThing(ctx context.Context, in *ThingIDRequest, opts ...grpc.CallOption) (*google_protobuf1.Empty, error)
@@ -151,16 +178,48 @@ func NewThingSvcClient(cc *grpc.ClientConn) ThingSvcClient {
 
 func (c *thingSvcClient) GetThing(ctx context.Context, in *ThingIDRequest, opts ...grpc.CallOption) (*Thing, error) {
 	out := new(Thing)
-	err := grpc.Invoke(ctx, "/proto.ThingSvc/GetThing", in, out, c.cc, opts...)
+	err := grpc.Invoke(ctx, "/iochti.thing.ThingSvc/GetThing", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
+func (c *thingSvcClient) ListGroupThings(ctx context.Context, in *GroupIDRequest, opts ...grpc.CallOption) (ThingSvc_ListGroupThingsClient, error) {
+	stream, err := grpc.NewClientStream(ctx, &_ThingSvc_serviceDesc.Streams[0], c.cc, "/iochti.thing.ThingSvc/ListGroupThings", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &thingSvcListGroupThingsClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type ThingSvc_ListGroupThingsClient interface {
+	Recv() (*Thing, error)
+	grpc.ClientStream
+}
+
+type thingSvcListGroupThingsClient struct {
+	grpc.ClientStream
+}
+
+func (x *thingSvcListGroupThingsClient) Recv() (*Thing, error) {
+	m := new(Thing)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 func (c *thingSvcClient) CreateThing(ctx context.Context, in *Thing, opts ...grpc.CallOption) (*Thing, error) {
 	out := new(Thing)
-	err := grpc.Invoke(ctx, "/proto.ThingSvc/CreateThing", in, out, c.cc, opts...)
+	err := grpc.Invoke(ctx, "/iochti.thing.ThingSvc/CreateThing", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -169,7 +228,7 @@ func (c *thingSvcClient) CreateThing(ctx context.Context, in *Thing, opts ...grp
 
 func (c *thingSvcClient) UpdateThing(ctx context.Context, in *Thing, opts ...grpc.CallOption) (*Thing, error) {
 	out := new(Thing)
-	err := grpc.Invoke(ctx, "/proto.ThingSvc/UpdateThing", in, out, c.cc, opts...)
+	err := grpc.Invoke(ctx, "/iochti.thing.ThingSvc/UpdateThing", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -178,7 +237,7 @@ func (c *thingSvcClient) UpdateThing(ctx context.Context, in *Thing, opts ...grp
 
 func (c *thingSvcClient) DeleteThing(ctx context.Context, in *ThingIDRequest, opts ...grpc.CallOption) (*google_protobuf1.Empty, error) {
 	out := new(google_protobuf1.Empty)
-	err := grpc.Invoke(ctx, "/proto.ThingSvc/DeleteThing", in, out, c.cc, opts...)
+	err := grpc.Invoke(ctx, "/iochti.thing.ThingSvc/DeleteThing", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -187,7 +246,7 @@ func (c *thingSvcClient) DeleteThing(ctx context.Context, in *ThingIDRequest, op
 
 func (c *thingSvcClient) BulkDeleteThing(ctx context.Context, in *ThingIDArray, opts ...grpc.CallOption) (*google_protobuf1.Empty, error) {
 	out := new(google_protobuf1.Empty)
-	err := grpc.Invoke(ctx, "/proto.ThingSvc/BulkDeleteThing", in, out, c.cc, opts...)
+	err := grpc.Invoke(ctx, "/iochti.thing.ThingSvc/BulkDeleteThing", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -198,6 +257,7 @@ func (c *thingSvcClient) BulkDeleteThing(ctx context.Context, in *ThingIDArray, 
 
 type ThingSvcServer interface {
 	GetThing(context.Context, *ThingIDRequest) (*Thing, error)
+	ListGroupThings(*GroupIDRequest, ThingSvc_ListGroupThingsServer) error
 	CreateThing(context.Context, *Thing) (*Thing, error)
 	UpdateThing(context.Context, *Thing) (*Thing, error)
 	DeleteThing(context.Context, *ThingIDRequest) (*google_protobuf1.Empty, error)
@@ -218,12 +278,33 @@ func _ThingSvc_GetThing_Handler(srv interface{}, ctx context.Context, dec func(i
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/proto.ThingSvc/GetThing",
+		FullMethod: "/iochti.thing.ThingSvc/GetThing",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(ThingSvcServer).GetThing(ctx, req.(*ThingIDRequest))
 	}
 	return interceptor(ctx, in, info, handler)
+}
+
+func _ThingSvc_ListGroupThings_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(GroupIDRequest)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(ThingSvcServer).ListGroupThings(m, &thingSvcListGroupThingsServer{stream})
+}
+
+type ThingSvc_ListGroupThingsServer interface {
+	Send(*Thing) error
+	grpc.ServerStream
+}
+
+type thingSvcListGroupThingsServer struct {
+	grpc.ServerStream
+}
+
+func (x *thingSvcListGroupThingsServer) Send(m *Thing) error {
+	return x.ServerStream.SendMsg(m)
 }
 
 func _ThingSvc_CreateThing_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -236,7 +317,7 @@ func _ThingSvc_CreateThing_Handler(srv interface{}, ctx context.Context, dec fun
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/proto.ThingSvc/CreateThing",
+		FullMethod: "/iochti.thing.ThingSvc/CreateThing",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(ThingSvcServer).CreateThing(ctx, req.(*Thing))
@@ -254,7 +335,7 @@ func _ThingSvc_UpdateThing_Handler(srv interface{}, ctx context.Context, dec fun
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/proto.ThingSvc/UpdateThing",
+		FullMethod: "/iochti.thing.ThingSvc/UpdateThing",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(ThingSvcServer).UpdateThing(ctx, req.(*Thing))
@@ -272,7 +353,7 @@ func _ThingSvc_DeleteThing_Handler(srv interface{}, ctx context.Context, dec fun
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/proto.ThingSvc/DeleteThing",
+		FullMethod: "/iochti.thing.ThingSvc/DeleteThing",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(ThingSvcServer).DeleteThing(ctx, req.(*ThingIDRequest))
@@ -290,7 +371,7 @@ func _ThingSvc_BulkDeleteThing_Handler(srv interface{}, ctx context.Context, dec
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/proto.ThingSvc/BulkDeleteThing",
+		FullMethod: "/iochti.thing.ThingSvc/BulkDeleteThing",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(ThingSvcServer).BulkDeleteThing(ctx, req.(*ThingIDArray))
@@ -299,7 +380,7 @@ func _ThingSvc_BulkDeleteThing_Handler(srv interface{}, ctx context.Context, dec
 }
 
 var _ThingSvc_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "proto.ThingSvc",
+	ServiceName: "iochti.thing.ThingSvc",
 	HandlerType: (*ThingSvcServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
@@ -323,33 +404,41 @@ var _ThingSvc_serviceDesc = grpc.ServiceDesc{
 			Handler:    _ThingSvc_BulkDeleteThing_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "ListGroupThings",
+			Handler:       _ThingSvc_ListGroupThings_Handler,
+			ServerStreams: true,
+		},
+	},
 	Metadata: "thing.proto",
 }
 
-func init() { proto1.RegisterFile("thing.proto", fileDescriptor0) }
+func init() { proto.RegisterFile("thing.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 325 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x92, 0xc1, 0x4e, 0xb3, 0x40,
-	0x14, 0x85, 0x3b, 0xb4, 0x34, 0xed, 0xa5, 0xe9, 0x9f, 0xcc, 0x1f, 0x1b, 0x82, 0x0b, 0x27, 0x2c,
-	0x0c, 0x89, 0x09, 0x8d, 0x75, 0xe5, 0xc6, 0x58, 0xc5, 0x98, 0x6e, 0xb1, 0xae, 0x0d, 0x6d, 0xaf,
-	0x95, 0x58, 0x0a, 0xc2, 0xc5, 0xa4, 0x8f, 0xe2, 0xf3, 0xf8, 0x62, 0x86, 0x19, 0x48, 0xa0, 0xda,
-	0xe8, 0x0a, 0xe6, 0xde, 0xef, 0x1c, 0xce, 0x01, 0xc0, 0xa0, 0x97, 0x70, 0xbb, 0x76, 0x93, 0x34,
-	0xa6, 0x98, 0xeb, 0xf2, 0x62, 0x9d, 0xac, 0xe3, 0x78, 0xbd, 0xc1, 0xb1, 0x3c, 0x2d, 0xf2, 0xe7,
-	0x31, 0x85, 0x11, 0x66, 0x14, 0x44, 0x89, 0xe2, 0xac, 0xe3, 0x7d, 0x00, 0xa3, 0x84, 0x76, 0x6a,
-	0x69, 0x9f, 0xc2, 0x60, 0x5e, 0x78, 0xce, 0xbc, 0x69, 0x9a, 0x06, 0x3b, 0x3e, 0x82, 0xae, 0x7c,
-	0x46, 0x66, 0x32, 0xd1, 0x76, 0x74, 0xbf, 0x3c, 0xd9, 0x9f, 0x0c, 0x74, 0x09, 0xf2, 0x21, 0x68,
-	0x33, 0xcf, 0x64, 0x82, 0x39, 0xba, 0xaf, 0xcd, 0x3c, 0xce, 0xa1, 0xb3, 0x0d, 0x22, 0x34, 0x35,
-	0xc1, 0x9c, 0xbe, 0x2f, 0xef, 0xb9, 0x00, 0x63, 0x85, 0xd9, 0x32, 0x0d, 0x13, 0x0a, 0xe3, 0xad,
-	0xd9, 0x96, 0xab, 0xfa, 0x88, 0x5f, 0x02, 0x2c, 0x53, 0x0c, 0x08, 0x57, 0x4f, 0x01, 0x99, 0x1d,
-	0xc1, 0x1c, 0x63, 0x62, 0xb9, 0x2a, 0xa9, 0x5b, 0x25, 0x75, 0xe7, 0x55, 0x15, 0xbf, 0x5f, 0xd2,
-	0x53, 0x2a, 0xa4, 0x79, 0xb2, 0xaa, 0xa4, 0xfa, 0xef, 0xd2, 0x92, 0x9e, 0x92, 0x2d, 0x60, 0x58,
-	0xb6, 0xf5, 0xf1, 0x2d, 0xc7, 0x8c, 0xf6, 0xdb, 0x4c, 0x3e, 0x34, 0xe8, 0x49, 0xe4, 0xe1, 0x7d,
-	0xc9, 0xcf, 0xa1, 0x77, 0x8f, 0xa4, 0x6a, 0x1f, 0x29, 0x6b, 0xb7, 0xa9, 0xb7, 0x06, 0xf5, 0xb1,
-	0xdd, 0xe2, 0x67, 0x60, 0xdc, 0xca, 0xa4, 0x4a, 0xd5, 0x58, 0xff, 0x04, 0x3f, 0xca, 0x6c, 0x7f,
-	0x81, 0xaf, 0xc0, 0xf0, 0x70, 0x83, 0x15, 0x7c, 0x20, 0xcf, 0xe8, 0xdb, 0x8b, 0xb8, 0x2b, 0xbe,
-	0xb6, 0xdd, 0xe2, 0xd7, 0xf0, 0xef, 0x26, 0xdf, 0xbc, 0xd6, 0x3d, 0xfe, 0x37, 0x3d, 0xe4, 0x1f,
-	0x70, 0xd8, 0x61, 0xd1, 0x95, 0x93, 0x8b, 0xaf, 0x00, 0x00, 0x00, 0xff, 0xff, 0xfe, 0x8a, 0x24,
-	0x67, 0x86, 0x02, 0x00, 0x00,
+	// 367 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x52, 0xc1, 0x6e, 0xaa, 0x40,
+	0x14, 0x15, 0x11, 0x9f, 0x5e, 0x8c, 0xbe, 0xcc, 0x4b, 0xcc, 0x84, 0xd7, 0xa4, 0x84, 0x45, 0xc3,
+	0x6a, 0x6c, 0xec, 0xca, 0x55, 0x43, 0xc5, 0x58, 0x92, 0x76, 0x43, 0xed, 0xba, 0x41, 0x9d, 0x2a,
+	0xa9, 0x08, 0x85, 0xa1, 0x89, 0xdf, 0xd2, 0x6f, 0xea, 0x3f, 0x35, 0x5c, 0x20, 0x51, 0x2b, 0x6d,
+	0xba, 0x9b, 0x39, 0xf7, 0x9c, 0x93, 0x33, 0x67, 0x2e, 0xa8, 0x62, 0xed, 0x6f, 0x57, 0x2c, 0x8a,
+	0x43, 0x11, 0x92, 0x8e, 0x1f, 0x2e, 0xd6, 0xc2, 0x67, 0x88, 0x69, 0xe7, 0xab, 0x30, 0x5c, 0x6d,
+	0xf8, 0x00, 0x67, 0xf3, 0xf4, 0x79, 0x20, 0xfc, 0x80, 0x27, 0xc2, 0x0b, 0xa2, 0x9c, 0xae, 0xfd,
+	0x3f, 0x26, 0xf0, 0x20, 0x12, 0xbb, 0x7c, 0x68, 0x5c, 0x40, 0x67, 0x96, 0xd9, 0x38, 0xb6, 0x15,
+	0xc7, 0xde, 0x8e, 0xf4, 0xa1, 0x89, 0xb6, 0x09, 0x95, 0x74, 0xd9, 0x54, 0xdc, 0xe2, 0x66, 0x7c,
+	0x48, 0xa0, 0x20, 0x91, 0x74, 0xa1, 0xee, 0xd8, 0x54, 0xd2, 0x25, 0x53, 0x71, 0xeb, 0x8e, 0x4d,
+	0x28, 0xfc, 0x99, 0xc6, 0x61, 0x1a, 0x39, 0x36, 0xad, 0x23, 0x58, 0x5e, 0x09, 0x81, 0xc6, 0xd6,
+	0x0b, 0x38, 0x95, 0x75, 0xc9, 0x6c, 0xbb, 0x78, 0x26, 0x7f, 0x41, 0xbe, 0xb7, 0xc6, 0xb4, 0x81,
+	0x50, 0x76, 0x24, 0x23, 0x80, 0x45, 0xcc, 0x3d, 0xc1, 0x97, 0x4f, 0x9e, 0xa0, 0x8a, 0x2e, 0x99,
+	0xea, 0x50, 0x63, 0x79, 0x66, 0x56, 0x66, 0x66, 0xb3, 0xf2, 0x51, 0x6e, 0xbb, 0x60, 0x5b, 0x22,
+	0x93, 0xa6, 0xd1, 0xb2, 0x94, 0x36, 0x7f, 0x96, 0x16, 0x6c, 0x4b, 0x18, 0x3a, 0x74, 0x8b, 0x98,
+	0x2e, 0x7f, 0x4d, 0x79, 0x22, 0x8e, 0xdf, 0x95, 0x31, 0x8a, 0x66, 0x2a, 0x18, 0xc3, 0x77, 0x19,
+	0x5a, 0x48, 0x79, 0x78, 0x5b, 0x90, 0x6b, 0x68, 0x4d, 0xb9, 0xc8, 0x2b, 0x3a, 0x63, 0xfb, 0x3f,
+	0xc4, 0x0e, 0x6d, 0xb4, 0x7f, 0x27, 0xa6, 0x46, 0x8d, 0xdc, 0x42, 0xef, 0xce, 0x4f, 0x04, 0xa6,
+	0x42, 0x2c, 0x39, 0xf6, 0x39, 0x0c, 0x5c, 0xe1, 0x73, 0x29, 0x91, 0x11, 0xa8, 0x63, 0xec, 0x28,
+	0x4f, 0x73, 0x8a, 0x57, 0x15, 0x62, 0x04, 0xea, 0x23, 0x76, 0xf4, 0x7b, 0xe9, 0x04, 0x54, 0x9b,
+	0x6f, 0x78, 0x29, 0xfd, 0xbe, 0x83, 0xfe, 0x97, 0x5f, 0x9a, 0x64, 0x4b, 0x69, 0xd4, 0xc8, 0x14,
+	0x7a, 0x37, 0xe9, 0xe6, 0x65, 0xdf, 0x4a, 0x3b, 0x69, 0x85, 0xfb, 0x5a, 0x6d, 0x34, 0x6f, 0x22,
+	0x72, 0xf5, 0x19, 0x00, 0x00, 0xff, 0xff, 0x68, 0xd2, 0xcb, 0x3d, 0x3b, 0x03, 0x00, 0x00,
 }
